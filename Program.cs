8769d1c25d941d00c7a4a63f3cf05714dcbbe6ff
vchavauty <v.chavauty@gmail.com>
2021-04-03 10:01:46 -0300

    created base directory!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
